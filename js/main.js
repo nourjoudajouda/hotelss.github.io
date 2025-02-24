81ea96b38f7e80f98eb0d507c7daf050e4091b64
nourjoudajouda <80156562+nourjoudajouda@users.noreply.github.com>
2025-02-24 12:19:46 +0200

    final_updates

--- js/main.js
+++ js/main.js
@@ -108,6 +108,45 @@ const swiperFeatures = new Swiper(".swiper-features", {
   },
 });
 
+
+// Initialize the Features Swiper
+const swiperSecondFeatures = new Swiper(".swiper-second-features", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  infinite: true,
+  initialSlide: 1, // Start from the second slide
+  spaceBetween: 30,
+  // loop: true,
+  // autoplay: {
+  //   delay: 3000, // Time between slides (in milliseconds)
+  //   disableOnInteraction: false, // Keeps autoplay running after user interaction
+  // },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 // Initialize the Reviews Swiper
 const swiperReviews = new Swiper(".swiper-reviews", {
   slidesPerView: 3,
